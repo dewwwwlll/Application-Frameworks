6e23734920cb9817ba16a0a6286a9e7062aa7cd3
dewwwwlll <denukasachith@gmail.com>
2019-04-08 15:05:03 +0530

    Lab Sheet 03 upto question 8

---
+++ ReactLab/AppContainer.jsx
@@ -0,0 +1,16 @@
+'use strict';
+import React, {Component} from 'react';
+
+export default class AppContainer extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div>
+            <h1>Hello, world!</h1>
+        </div>;
+
+            }
+}
